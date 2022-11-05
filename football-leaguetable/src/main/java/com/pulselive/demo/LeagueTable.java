cfadf06f3ac7fe5901f8440a35691aa4f1370f6b
Sreelekha0413 <117518295+Sreelekha0413@users.noreply.github.com>
2022-11-05 14:03:32 +0000

    Football League Java Maven project
    Followed TDD approach for football league project.

---
+++ football-leaguetable/src/main/java/com/pulselive/demo/LeagueTable.java
@@ -0,0 +1,19 @@
+package com.pulselive.demo;
+
+import java.util.List;
+
+import com.pulselive.demo.model.LeagueTableEntry;
+import com.pulselive.demo.model.Match;
+
+public class LeagueTable {
+
+    public LeagueTable(final List<Match> matches) {
+
+    }
+
+    public List<LeagueTableEntry> getTableEntries() {
+    	List<LeagueTableEntry> leagueTable = null;
+    	return leagueTable;
+
+    }
+}
---
+++ football-leaguetable/src/test/java/com/pulselive/demo/LeagueTableTest.java
@@ -0,0 +1,56 @@
+package com.pulselive.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.pulselive.demo.model.LeagueTableEntry;
+import com.pulselive.demo.model.Match;
+
+public class LeagueTableTest {
+	@Test
+	public void TestLeagueTable_Entries() {
+		List<Match> matches = new ArrayList<Match>();
+		matches.add(new Match("Manchester United","Liverpool F.C",5,2));
+		matches.add(new Match("Liverpool F.C","Arsenal",2,4));
+		matches.add(new Match("Arsenal","Manchester United",2,2));
+		
+		List<LeagueTableEntry> leagueTableentries = new LeagueTable(matches).getTableEntries();
+		assertEquals("Manchester United", leagueTableentries.get(0).getTeamName());
+		assertEquals(2, leagueTableentries.get(0).getPlayed());
+		assertEquals(1, leagueTableentries.get(0).getWon());
+		assertEquals(1, leagueTableentries.get(0).getDrawn());
+		assertEquals(0, leagueTableentries.get(0).getLost());
+		assertEquals(7, leagueTableentries.get(0).getGoalsFor());
+		assertEquals(4, leagueTableentries.get(0).getGoalsAgainst());
+		assertEquals(3, leagueTableentries.get(0).getGoalDifference());
+		assertEquals(4, leagueTableentries.get(0).getPoints());
+		
+		assertEquals("Arsenal", leagueTableentries.get(1).getTeamName());
+		assertEquals(2, leagueTableentries.get(1).getPlayed());
+		assertEquals(1, leagueTableentries.get(1).getWon());
+		assertEquals(1, leagueTableentries.get(1).getDrawn());
+		assertEquals(0, leagueTableentries.get(1).getLost());
+		assertEquals(6, leagueTableentries.get(1).getGoalsFor());
+		assertEquals(4, leagueTableentries.get(1).getGoalsAgainst());
+		assertEquals(2, leagueTableentries.get(1).getGoalDifference());
+		assertEquals(4, leagueTableentries.get(1).getPoints());
+		
+		assertEquals("Liverpool F.C", leagueTableentries.get(2).getTeamName());
+		assertEquals(2, leagueTableentries.get(2).getPlayed());
+		assertEquals(0, leagueTableentries.get(2).getWon());
+		assertEquals(0, leagueTableentries.get(2).getDrawn());
+		assertEquals(2, leagueTableentries.get(2).getLost());
+		assertEquals(4, leagueTableentries.get(2).getGoalsFor());
+		assertEquals(9, leagueTableentries.get(2).getGoalsAgainst());
+		assertEquals(-5, leagueTableentries.get(2).getGoalDifference());
+		assertEquals(0, leagueTableentries.get(2).getPoints());
+		
+		
+		
+	}
+
+}
